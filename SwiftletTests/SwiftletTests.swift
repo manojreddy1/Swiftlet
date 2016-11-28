ccfec695267293b590c575e3d7fe5039012457b2
Manoj <manoj@Manojs-MacBook-Pro.local>
2016-11-28 21:31:05 +0530

    Initial Commit

---
+++ SwiftletTests/SwiftletTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftletTests.swift
+//  SwiftletTests
+//
+//  Created by Manoj on 28/11/16.
+//  Copyright © 2016 Manoj. All rights reserved.
+//
+
+import XCTest
+@testable import Swiftlet
+
+class SwiftletTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
