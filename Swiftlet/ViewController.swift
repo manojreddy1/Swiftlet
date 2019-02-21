63d3c68f1020e0e6d94bf28858f7cc1dcd8b9868
Manoj Reddy <vaayoo@vaayoos-macbook-pro.local>
2019-02-21 18:17:14 +0530

    Manoj commit

--- Swiftlet/ViewController.swift
+++ Swiftlet/ViewController.swift
@@ -9,20 +9,19 @@
 import UIKit
 let manoj = "manoj"
 let manoj1 = "manoj"
-
+let version = 3.2
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("ok")
-            }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
